7bdc17fddc21a3747c10930982ce0831c3f6fa20
Gilvam Mourão <gilvamm@gmail.com>
2019-04-21 00:07:57 -0300

    Criando formGroup para controlar o formulário

--- src/app/sign-up/sign-up.page.ts
+++ src/app/sign-up/sign-up.page.ts
@@ -10,16 +10,35 @@ import { ClientService } from '../../services/domain/client.service';
 })
 export class SignUpPage implements OnInit {
 
+  formGroup: FormGroup;
 
   constructor(
+    public navCtrl: NavController,
+    public formBuilder: FormBuilder,
   ) {
+    this.formGroup = this.formBuilder.group({
+      name: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      type: ['1', [Validators.required]],
+      cpfOrCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      password: ['123', [Validators.required]],
+      addressName: ['Rua Via', [Validators.required]],
+      number: ['25', [Validators.required]],
+      complement: ['Apto 3', []],
+      district: ['Copacabana', []],
+      zipCode: ['10828333', [Validators.required]],
+      phone1: ['977261827', [Validators.required]],
+      phone2: ['', []],
+      phone3: ['', []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]]
+    });
   }
 
   ngOnInit() {
   }
 
   signupUser() {
-    console.log('enviou o form');
   }
 
 
